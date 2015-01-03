463d54d1ea537f29900175d4b597e4b2b0d1dceb
Alon Heller <alon.heller@gmail.com>
2015-01-04 00:14:50 +0200

    Fields added

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,13 +7,21 @@ angular.module('ksRealEstate')
                 alert('id: ' + newValue);
             }
         });
-        realEstateService.gettingList().then(
-            function success(result) {
-                var arr = [];
-                angular.forEach(result, function (value, key) {
-                    arr.push({price: value.toJSON().price})
-                });
-                $scope.realEstates = arr;
-            }
-        );
+    realEstateService.gettingList().then(
+      function success(result) {
+        var arr = [];
+        angular.forEach(result, function (value, key) {
+          var apartment = {};
+          apartment.price = value.toJSON().price;
+          apartment.airConditioning = value.toJSON().Air_Conditioning;
+          apartment.area = value.toJSON().Area;
+          apartment.city = value.toJSON().city;
+          apartment.neighborhood = value.toJSON().Neighborhood;
+          apartment.propertyType = value.toJSON().Property_Type;
+          apartment.houseSize = value.toJSON().House_Size;
+          arr.push(apartment);
+        });
+        $scope.realEstates = arr;
+      }
+    );
   });
